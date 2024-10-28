19ac12f6ce7a320c569d5340d8296042b6622c02
stackedstewart <jclstewart87@gmail.com>
2024-10-28 14:33:07 +0000

    newbranch

--- SwitchOperatedTextMenu.java
+++ SwitchOperatedTextMenu.java
@@ -4,7 +4,7 @@ public class SwitchOperatedTextMenu {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		// print menu
-		for (int i = 1; i <= 2; i++)
+		for (int i = 1; i <= 3; i++)
 			System.out.println(i + ". Menu item #" + i);
 		System.out.println("0. Quit");
 		// handle user commands
@@ -21,6 +21,10 @@ public class SwitchOperatedTextMenu {
 			case 2:
 				System.out.println("You've chosen item #2");
 				// do something...
+				break;
+			case 3:
+				System.out.println("You've chosen item #3");
+				// Do something...
 				break;
 			case 0:
 				quit = true;
